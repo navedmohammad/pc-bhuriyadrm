ec228fbfe8fe69fd244005b1fcf7dbeb923a465c
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-11-27 00:47:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pc-bhuriyadrm
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/bhuriya12/drmtxtbot)
---
+++ vars.py
@@ -0,0 +1,5 @@
+api_id = 22475741
+api_hash = "1a217be71a0225e0a678af286c211f8a"
+bot_token = "7086426763:AAFAbz6xw3u0F7EfQnpeZ0nmeOloLkpvKC0"
+AUTH = 6562642609
+#Bot Username = "bhuriyabhaig"
